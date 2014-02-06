a9e8c8fe7f5c6c4aad48fbc5190402043c3758f6
Mateusz Paprocki <mattpap@gmail.com>
2014-02-06 19:29:27 +0100

    Move mixins to Mixins.scala

--- src/main/scala/Props.scala
+++ src/main/scala/Props.scala
@@ -1,30 +1,3 @@
 package org.continuumio.bokeh
 
-class DataSpec[T] {
-    case class DataValue(name: String, units: Units, default: T)
-}
 
-trait FillProps { self: PlotObject =>
-    //val fill_color = ColorSpec("gray")
-    //val fill_alpha = Percent(1.0)
-}
-
-trait LineProps { self: PlotObject =>
-    //val line_color = ColorSpec("black")
-    //val line_width = Size(1)
-    //val line_alpha = Percent(1.0)
-    //val line_join = Enum[LineJoin]
-    //val line_cap = Enum[LineCap]
-    //val line_dash = Pattern
-    //val line_dash_offset = Int(0)
-}
-
-trait TextProps { self: PlotObject =>
-    //val text_font = String
-    //val text_font_size = String("10pt")
-    //val text_font_style = Enum[FontStyle]
-    //val text_color = Color("black")
-    //val text_alpha = Percent(1.0)
-    //val text_align = Enum[TextAlign]
-    //val text_baseline = Enum[Baseline]
-}
