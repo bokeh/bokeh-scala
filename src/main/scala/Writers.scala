c434f78f096828c86c9c69da63acb08e79dc8bb9
Mateusz Paprocki <mattpap@gmail.com>
2014-02-07 12:34:17 +0100

    Add stub implementation of JSON formats

---
+++ src/main/scala/Writers.scala
@@ -0,0 +1,66 @@
+package org.continuumio.bokeh
+
+/*
+object Formats {
+    implicit val AnnularWedgeJSON = Json.format[AnnularWedge]
+    implicit val AnnulusJSON = Json.format[Annulus]
+    implicit val ArcJSON = Json.format[Arc]
+    implicit val BezierJSON = Json.format[Bezier]
+    implicit val ImageJSON = Json.format[Image]
+    implicit val ImageURIJSON = Json.format[ImageURI]
+    implicit val ImageRGBAJSON = Json.format[ImageRGBA]
+    implicit val LineJSON = Json.format[Line]
+    implicit val MultiLineJSON = Json.format[MultiLine]
+    implicit val OvalJSON = Json.format[Oval]
+    implicit val PatchJSON = Json.format[Patch]
+    implicit val PatchesJSON = Json.format[Patches]
+    implicit val QuadJSON = Json.format[Quad]
+    implicit val QuadraticJSON = Json.format[Quadratic]
+    implicit val RayJSON = Json.format[Ray]
+    implicit val RectJSON = Json.format[Rect]
+    implicit val SegmentJSON = Json.format[Segment]
+    implicit val TextJSON = Json.format[Text]
+    implicit val WedgeJSON = Json.format[Wedge]
+
+    implicit val LinearAxisJSON = Json.format[LinearAxis]
+    implicit val DatetimeAxisJSON = Json.format[DatetimeAxis]
+    implicit val GridJSON = Json.format[Grid]
+
+    implicit val CircleJSON = Json.format[Circle]
+    implicit val SquareJSON = Json.format[Square]
+    implicit val TriangleJSON = Json.format[Triangle]
+    implicit val CrossJSON = Json.format[Cross]
+    implicit val XmarkerJSON = Json.format[Xmarker]
+    implicit val DiamondJSON = Json.format[Diamond]
+    implicit val InvertedTriangleJSON = Json.format[InvertedTriangle]
+    implicit val SquareXJSON = Json.format[SquareX]
+    implicit val AsteriskJSON = Json.format[Asterisk]
+    implicit val DiamondCrossJSON = Json.format[DiamondCross]
+    implicit val CircleCrossJSON = Json.format[CircleCross]
+    implicit val HexStarJSON = Json.format[HexStar]
+    implicit val SquareCrossJSON = Json.format[SquareCross]
+    implicit val CircleXJSON = Json.format[CircleX]
+
+    implicit val PlotContextJSON = Json.format[PlotContext]
+
+    implicit val PlotJSON = Json.format[Plot]
+    implicit val GridPlotJSON = Json.format[GridPlot]
+
+    implicit val Range1dJSON = Json.format[Range1d]
+    implicit val DataRange1dJSON = Json.format[DataRange1d]
+
+    implicit val GlyphJSON = Json.format[Glyph]
+
+    implicit val ColumnDataSourceJSON = Json.format[ColumnDataSource]
+
+    implicit val PanToolJSON = Json.format[PanTool]
+    implicit val WheelZoomToolJSON = Json.format[WheelZoomTool]
+    implicit val PreviewSaveToolJSON = Json.format[PreviewSaveTool]
+    implicit val EmbedToolJSON = Json.format[EmbedTool]
+    implicit val ResetToolJSON = Json.format[ResetTool]
+    implicit val ResizeToolJSON = Json.format[ResizeTool]
+    implicit val CrosshairToolJSON = Json.format[CrosshairTool]
+    implicit val BoxZoomToolJSON = Json.format[BoxZoomTool]
+    implicit val BoxSelectToolJSON = Json.format[BoxSelectTool]
+}
+*/
