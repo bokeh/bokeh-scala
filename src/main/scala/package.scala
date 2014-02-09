9dd4be4513592df4d9da17a8cb77122b38c354a5
Mateusz Paprocki <mattpap@gmail.com>
2014-02-09 22:17:38 +0100

    Add snakify()

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -10,4 +10,9 @@ package object bokeh {
     }
 
     def uuid4(): String = java.util.UUID.randomUUID.toString
+
+    def snakify(name: String): String =
+        name.replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2")
+            .replaceAll("([a-z\\d])([A-Z])", "$1_$2")
+            .toLowerCase
 }
