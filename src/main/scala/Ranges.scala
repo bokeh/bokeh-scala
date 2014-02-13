0532544b9f15a3d046fcce1cd22dce251d40d004
Mateusz Paprocki <mattpap@gmail.com>
2014-02-13 12:58:42 +0100

    Implement ColumnRef and add DataSource.columns()

--- src/main/scala/Ranges.scala
+++ src/main/scala/Ranges.scala
@@ -7,11 +7,17 @@ class Range1d extends Range {
     object end extends Field[this.type, Double](this)
 }
 
+class ColumnsRef extends PlotObject with NoRefs {
+    object ref extends Field[this.type, DataSource](this)
+    object columns extends Field[this.type, List[String]](this)
+}
+
 trait DataRange { self: Range => }
 
 class DataRange1d extends Range with DataRange {
     object start extends Field[this.type, Double](this)
     object end extends Field[this.type, Double](this)
-    //object sources extends Field[List[ColumnsRef]]
+
+    object sources extends Field[this.type, List[ColumnsRef]](this)
     object rangepadding extends Field[this.type, Double](this, 0.1)
 }
--- src/main/scala/Sources.scala
+++ src/main/scala/Sources.scala
@@ -5,6 +5,9 @@ import breeze.linalg.DenseVector
 sealed abstract class DataSource extends PlotObject {
     object column_names extends Field[this.type, List[String]](this)
     object selected extends Field[this.type, List[String]](this)
+
+    def columns(columns: String*): ColumnsRef =
+        new ColumnsRef().ref(this).columns(columns.toList)
 }
 
 class ColumnDataSource extends DataSource {
