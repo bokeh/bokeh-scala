eeded474edc3f09121c752aaa19a8cb61b4c69da
Mateusz Paprocki <mattpap@gmail.com>
2014-06-12 15:12:42 +0200

    Add default values for java.util.Date and Tuple{2,3}

--- src/main/scala/Defaults.scala
+++ src/main/scala/Defaults.scala
@@ -1,16 +1,31 @@
 package org.continuumio.bokeh
 
-import annotation.implicitNotFound
+import scala.annotation.implicitNotFound
+import java.util.Date
 
 @implicitNotFound(msg="Can't find DefaultValue type class for type ${T}.")
 class DefaultValue[T](val default: T)
 
-trait DefaultImplicits {
+trait TypeDefaults {
     implicit object BooleanDefault extends DefaultValue[Boolean](false)
     implicit object IntDefault extends DefaultValue[Int](0)
     implicit object DoubleDefault extends DefaultValue[Double](0.0)
     implicit object StringDefault extends DefaultValue[String]("")
     implicit object PercentDefault extends DefaultValue[Percent](100%%)
+    implicit object DateDefault extends DefaultValue[Date](new Date)
+
+    implicit def SeqDefault[T]: DefaultValue[Seq[T]] = new DefaultValue[Seq[T]](Seq())
+    implicit def ListDefault[T]: DefaultValue[List[T]] = new DefaultValue[List[T]](Nil)
+    implicit def MapDefault[U, V]: DefaultValue[Map[U, V]] = new DefaultValue[Map[U, V]](Map.empty)
+    implicit def HasFieldsDefault[T <: HasFields]: DefaultValue[T] = new DefaultValue[T](null.asInstanceOf[T])
+
+    implicit def Tuple2Default[T1:DefaultValue, T2:DefaultValue]: DefaultValue[(T1, T2)] =
+        new DefaultValue[(T1, T2)]((implicitly[DefaultValue[T1]].default, implicitly[DefaultValue[T2]].default))
+    implicit def Tuple3Default[T1:DefaultValue, T2:DefaultValue, T3:DefaultValue]: DefaultValue[(T1, T2, T3)] =
+        new DefaultValue[(T1, T2, T3)]((implicitly[DefaultValue[T1]].default, implicitly[DefaultValue[T2]].default, implicitly[DefaultValue[T3]].default))
+}
+
+trait EnumDefaults {
     implicit object LineJoinDefault extends DefaultValue[LineJoin](LineJoin.Miter)
     implicit object LineDashDefault extends DefaultValue[LineDash](LineDash.Solid)
     implicit object LineCapDefault extends DefaultValue[LineCap](LineCap.Butt)
@@ -29,10 +44,7 @@ trait DefaultImplicits {
     implicit object DashPatternDefault extends DefaultValue[DashPattern](DashPattern.Solid)
     implicit object AnchorDefault extends DefaultValue[Anchor](Anchor.TopLeft)
     implicit object ColorDefault extends DefaultValue[Color](Color.White)
-    implicit def SeqDefault[T]: DefaultValue[Seq[T]] = new DefaultValue[Seq[T]](Seq())
-    implicit def ListDefault[T]: DefaultValue[List[T]] = new DefaultValue[List[T]](Nil)
-    implicit def MapDefault[U, V]: DefaultValue[Map[U, V]] = new DefaultValue[Map[U, V]](Map.empty)
-    implicit def HasFieldsDefault[T <: HasFields]: DefaultValue[T] = new DefaultValue[T](null.asInstanceOf[T])
 }
 
+trait DefaultImplicits extends TypeDefaults with EnumDefaults
 object DefaultImplicits extends DefaultImplicits
