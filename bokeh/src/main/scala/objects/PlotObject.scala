0cefb9837494065b61c7e0c037a6cd48054c3fae
Mateusz Paprocki <mattpap@gmail.com>
2014-08-25 00:10:26 +0200

    Simple abstraction related to refs

--- bokeh/src/main/scala/objects/PlotObject.scala
+++ bokeh/src/main/scala/objects/PlotObject.scala
@@ -2,8 +2,7 @@ package io.continuum.bokeh
 
 case class Ref(id: String, `type`: String)
 
-abstract class PlotObject extends HasFields {
+abstract class PlotObject extends HasFields with Refs[Ref] {
     def getRef: Ref = Ref(id.value, typeName)
-
     object id extends Field[String](IdGenerator.next())
 }
--- core/src/main/scala/Fields.scala
+++ core/src/main/scala/Fields.scala
@@ -3,5 +3,13 @@ package io.continuum.bokeh
 trait AbstractField {
     type ValueType
 
+    def valueOpt: Option[ValueType]
+    def value: ValueType
+
     def set(value: Option[ValueType])
+}
+
+trait Refs[Ref] {
+    def getRef: Ref
+    def id: AbstractField { type ValueType = String }
 }
