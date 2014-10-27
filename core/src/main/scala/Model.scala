10c9529725c7e5639f3d7210bcc7a138124722e8
Mateusz Paprocki <mattpap@gmail.com>
2014-10-27 14:01:13 +0100

    Make included properties work in Scala 2.10

--- core/src/main/scala/Model.scala
+++ core/src/main/scala/Model.scala
@@ -11,7 +11,8 @@ private object ModelImpl {
             case ClassDef(mods, name, tparams, tpl @ Template(parents, sf, body)) :: companion =>
                 val expandedBody = body.flatMap {
                     case q"$prefix = include[$mixin]" =>
-                        val tpe = c.typecheck(tq"$mixin", c.TYPEmode).tpe
+                        // XXX: should be c.typecheck(tq"$mixin", c.TYPEMODE)
+                        val tpe = c.typeCheck(q"null: $mixin").tpe
 
                         val fields = tpe.members
                             .filter(_.isModule)
@@ -22,6 +23,7 @@ private object ModelImpl {
                             val name = newTermName(s"${prefix}_${field.name}")
                             val sig = field.typeSignature
                             val tpe = sig.member(newTypeName("ValueType")).typeSignatureIn(sig)
+                            // TODO: add support for precise field type (Vectorized, NonNegative, etc.)
                             q"object $name extends Field[$tpe]"
                         }
                     case field => field :: Nil
