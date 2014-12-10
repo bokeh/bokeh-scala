1f38720116d11407f6fce0394f6e6d7c53a13557
Mateusz Paprocki <mattpap@gmail.com>
2014-12-10 20:56:27 +0100

    Change defs to vals in sampledata
    Data structures there are immutable, so this is allowed.

--- sampledata/src/main/scala/package.scala
+++ sampledata/src/main/scala/package.scala
@@ -1,20 +1,20 @@
 package io.continuum.bokeh {
     package sampledata {
         package object iris {
-            def flowers = Flowers.load()
+            val flowers = Flowers.load()
         }
 
         package object daylight {
-            def Warsaw2013 = Daylight.load("daylight_warsaw_2013.csv")
+            val Warsaw2013 = Daylight.load("daylight_warsaw_2013.csv")
         }
     }
 
     package object sampledata {
         val sprint = Sprint.load()
-        def autompg = AutoMPG.load()
-        def us_states = USStates.load()
-        def us_counties = USCounties.load()
-        def us_holidays = USHolidays.load()
-        def unemployment = Unemployment.load()
+        val autompg = AutoMPG.load()
+        val us_states = USStates.load()
+        val us_counties = USCounties.load()
+        val us_holidays = USHolidays.load()
+        val unemployment = Unemployment.load()
     }
 }
