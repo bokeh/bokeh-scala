5bbfea3183d5d21075b616437f34cfb64a00a233
Mateusz Paprocki <mattpap@gmail.com>
2015-01-15 22:22:47 +0100

    Remove ReactiveField

--- bokeh/src/main/scala/Fields.scala
+++ bokeh/src/main/scala/Fields.scala
@@ -5,12 +5,3 @@ trait ToolsField extends AbstractField { self: Plot#Field[List[Tool]] =>
         super.set(value.map(_.map(_.plot(owner))))
     }
 }
-
-trait ReactiveField[A] extends AbstractField { self: HasFields#Field[A] =>
-    abstract override def set(value: Option[A]) {
-        super.set(value)
-        value.map(value => reactors.foreach(_(value)))
-    }
-
-    val reactors: List[A => Unit]
-}
