141ca537be17e7329e6e44bf3bc5c79339b68f2a
Mateusz Paprocki <mattpap@gmail.com>
2015-08-05 01:14:44 +0200

    Make sure mixins match the reference implementation

--- bokeh/src/main/scala/Mixins.scala
+++ bokeh/src/main/scala/Mixins.scala
@@ -2,25 +2,25 @@ package io.continuum.bokeh
 
 trait FillProps { self: HasFields with Vectorization =>
     object fill_color extends Vectorized[Color](Color.Gray)
-    object fill_alpha extends Vectorized[Percent]
+    object fill_alpha extends Vectorized[Percent](1.0)
 }
 
 trait LineProps { self: HasFields with Vectorization =>
     object line_color extends Vectorized[Color](Color.Black)
     object line_width extends Vectorized[Double](1.0)
     object line_alpha extends Vectorized[Percent]
-    object line_join extends Vectorized[LineJoin]
-    object line_cap extends Vectorized[LineCap]
-    object line_dash extends Vectorized[List[Int]]
-    object line_dash_offset extends Vectorized[Int]
+    object line_join extends Field[LineJoin]
+    object line_cap extends Field[LineCap]
+    object line_dash extends Field[List[Int]]
+    object line_dash_offset extends Field[Int]
 }
 
 trait TextProps { self: HasFields with Vectorization =>
-    object text_font extends Vectorized[String]
-    object text_font_size extends Vectorized[FontSize](10 pt)
-    object text_font_style extends Vectorized[FontStyle]
-    object text_color extends Vectorized[Color](Color.Black)
+    object text_font extends Field[String]
+    object text_font_size extends Vectorized[FontSize](12 pt)
+    object text_font_style extends Field[FontStyle]
+    object text_color extends Vectorized[Color]("#444444")
     object text_alpha extends Vectorized[Percent]
-    object text_align extends Vectorized[TextAlign]
-    object text_baseline extends Vectorized[TextBaseline]
+    object text_align extends Field[TextAlign]
+    object text_baseline extends Field[TextBaseline]
 }
