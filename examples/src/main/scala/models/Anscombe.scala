9192ffcff3fa5754c92c1f8d8e9a719128edf3d5
Mateusz Paprocki <mattpap@gmail.com>
2016-02-07 20:36:14 +0100

    Don't use addColumn() in models/anscombe example

--- examples/src/main/scala/models/Anscombe.scala
+++ examples/src/main/scala/models/Anscombe.scala
@@ -5,29 +5,28 @@ package models
 import breeze.linalg.{DenseMatrix,linspace}
 
 object Anscombe extends Example {
-    val quartets = List('xi, 'yi, 'xii, 'yii, 'xiii, 'yiii, 'xiv, 'yiv)
+    val anscombe_quartet = DenseMatrix(
+        (10.0,  8.04, 10.0, 9.14, 10.0,  7.46,  8.0,  6.58),
+        ( 8.0,  6.95,  8.0, 8.14,  8.0,  6.77,  8.0,  5.76),
+        (13.0,  7.58, 13.0, 8.74, 13.0, 12.74,  8.0,  7.71),
+        ( 9.0,  8.81,  9.0, 8.77,  9.0,  7.11,  8.0,  8.84),
+        (11.0,  8.33, 11.0, 9.26, 11.0,  7.81,  8.0,  8.47),
+        (14.0,  9.96, 14.0, 8.10, 14.0,  8.84,  8.0,  7.04),
+        ( 6.0,  7.24,  6.0, 6.13,  6.0,  6.08,  8.0,  5.25),
+        ( 4.0,  4.26,  4.0, 3.10,  4.0,  5.39, 19.0, 12.50),
+        (12.0, 10.84, 12.0, 9.13, 12.0,  8.15,  8.0,  5.56),
+        ( 7.0,  4.82,  7.0, 7.26,  7.0,  6.42,  8.0,  7.91),
+        ( 5.0,  5.68,  5.0, 4.74,  5.0,  5.73,  8.0,  6.89))
 
-    val raw_columns = DenseMatrix(
-        (10.0,   8.04,   10.0,   9.14,   10.0,   7.46,   8.0,    6.58),
-        (8.0,    6.95,   8.0,    8.14,   8.0,    6.77,   8.0,    5.76),
-        (13.0,   7.58,   13.0,   8.74,   13.0,   12.74,  8.0,    7.71),
-        (9.0,    8.81,   9.0,    8.77,   9.0,    7.11,   8.0,    8.84),
-        (11.0,   8.33,   11.0,   9.26,   11.0,   7.81,   8.0,    8.47),
-        (14.0,   9.96,   14.0,   8.10,   14.0,   8.84,   8.0,    7.04),
-        (6.0,    7.24,   6.0,    6.13,   6.0,    6.08,   8.0,    5.25),
-        (4.0,    4.26,   4.0,    3.10,   4.0,    5.39,   19.0,   12.5),
-        (12.0,   10.84,  12.0,   9.13,   12.0,   8.15,   8.0,    5.56),
-        (7.0,    4.82,   7.0,    7.26,   7.0,    6.42,   8.0,    7.91),
-        (5.0,    5.68,   5.0,    4.74,   5.0,    5.73,   8.0,    6.89))
-
-    val data = quartets.zip(0 until raw_columns.cols).map {
-        case (quartet, i) => quartet -> raw_columns(::, i)
-    }
-
-    val circles_source = new ColumnDataSource()
-
-    data.foreach { case (name, array) =>
-        circles_source.addColumn(name, array)
+    object circles_source extends ColumnDataSource {
+        val xi   = column(anscombe_quartet(::, 0))
+        val yi   = column(anscombe_quartet(::, 1))
+        val xii  = column(anscombe_quartet(::, 2))
+        val yii  = column(anscombe_quartet(::, 3))
+        val xiii = column(anscombe_quartet(::, 4))
+        val yiii = column(anscombe_quartet(::, 5))
+        val xiv  = column(anscombe_quartet(::, 6))
+        val yiv  = column(anscombe_quartet(::, 7))
     }
 
     object lines_source extends ColumnDataSource {
